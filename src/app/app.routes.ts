737286f80eeff81d0a0553aab2abca7a44d49d59
Sean <131737726+Snance1985@users.noreply.github.com>
2024-05-07 15:01:50 -0700

    created routing for angular routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { OrderPageComponent } from './components/order-page/order-page.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { AddtoolPageComponent } from './components/addtool-page/addtool-page.component';
+import { SignupPageComponent } from './components/signup-page/signup-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "Order", pathMatch: "full" },
+    { path: "Order", component: OrderPageComponent },
+    { path: "Search", component: SearchPageComponent },
+    { path: "Login", component: LoginPageComponent },
+    { path: "Add Tool", component: AddtoolPageComponent },
+    { path: "Signup", component: SignupPageComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
